73d8a4b54a7016f79b6d6d7ad25f2774322cc976
asbrctl <55748433+asbrctl@users.noreply.github.com>
2020-06-02 13:27:53 -0300

    add aliases

--- linkFiles.sh
+++ linkFiles.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-## NAME: linkFiles
+#FNAME: linkFiles
 
 DOTFILEDIR=$HOME/dotfiles
+DOTFILEENV=$HOME/dotfiles/env
 
 
 if [ -d "$DOTFILEDIR" ]; then
@@ -9,10 +10,12 @@ if [ -d "$DOTFILEDIR" ]; then
   chmod go-rwx -R "$DOTFILEDIR"
 
   ## profile link
-  ln -sfn "${DOTFILEDIR}"/profile "$HOME/.profile"
+  ln -sfn "${DOTFILEENV}"/profile "$HOME/.profile"
 
   ## bash link
-  ln -sfn "${DOTFILEDIR}"/bash_profile "$HOME/.bash_profile"
+  ln -sfn "${DOTFILEENV}"/bash_profile "$HOME/.bash_profile"
+  ln -sfn "${DOTFILEENV}"/bashrc "$HOME/.bashrc"
+  ln -sfn "${DOTFILEENV}"/bash_aliases"$HOME/.bash_aliases"
 
   ## vim link
   ln -sfn "${DOTFILEDIR}"/vimrc "$HOME/.vimrc"
